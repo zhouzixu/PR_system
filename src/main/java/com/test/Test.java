74dd63be4e571a60ef4003bd80d4e08c049e099c
cath <386130939@qq.com>
2018-09-23 12:17:55 +0800

    update

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -5,7 +5,7 @@ import org.apache.log4j.PropertyConfigurator;
 
 public class Test {
     public static void main(String args[]){
-        PropertyConfigurator.configure("log4j.properties");
+        PropertyConfigurator.configure("property/log4j.properties");
         Logger logger=Logger.getLogger(Test.class);
         logger.debug("debug");
         logger.info("info");
